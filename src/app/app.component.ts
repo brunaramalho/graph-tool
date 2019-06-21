98722819d5a2ae323686b383c7fab7c4bc31295d
brunaramalho <ramalho.alves.bruna@gmail.com>
2019-06-20 21:44:23 -0300

    :construction: first page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'graph-tool';
+  options: FormGroup;
+
+  constructor(fb: FormBuilder) {
+    this.options = fb.group({
+      n: [3, [Validators.min(1), Validators.max(30)]],
+    });
+  }
+
+  algoritmo = {
+    value: "1"
+  }
+
+  lista = `1,2
+0
+0`
+  matriz = `0,1,1
+1,0,0
+1,0,0`
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonToggleModule} from '@angular/material';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonToggleModule,
+    MatRadioModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    FormsModule, ReactiveFormsModule,
+    MatInputModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
